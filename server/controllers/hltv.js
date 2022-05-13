5176b164a576fddb04c01dcea4fc6422f40053d4
Flickza <adelholmg@gmail.com>
2022-05-13 14:10:27 +0200

    hltv api

--- server/controllers/hltv.js
+++ server/controllers/hltv.js
@@ -1,10 +1,12 @@
-import hltv from 'hltv-api';
+import HLTVPkg from '../../node_modules/hltv-api/dist/index.js';
+const HLTV = HLTVPkg.default;
 
 export const getMatch = async (req, res) => {
     try {
+        const result = await HLTV.getMatches();
+        console.log(result);
 
-
-        // res.status(200).json(postMessages);
+        res.status(200).json(result);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
