92c3ae0410bdadf3a1b91d15f314491e80da9ad0
Flickza <adelholmg@gmail.com>
2022-05-14 13:32:16 +0200

    twitter posts work

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -1,3 +1,15 @@
+import Twit from 'twit';
+import keys from '../config.js';
+
+
+var T = new Twit({
+    consumer_key: keys.API_KEY,
+    consumer_secret: keys.API_KEY_SECRET,
+    access_token: keys.ACCESS_TOKEN,
+    access_token_secret: keys.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000,
+    strictSSL: true
+})
 
 export const getTweet = async (req, res) => {
     try {
@@ -9,8 +21,18 @@ export const getTweet = async (req, res) => {
 
 export const createTweet = async (req, res) => {
     try {
-
         res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const testTweet = async (req, res) => {
+    try {
+        T.post('statuses/update', { status: 'Hello World2!' }, function(err, data, response) {
+            console.log(data);
+            res.status(200).json(data);
+        })
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
--- server/routes/twitterRoutes.js
+++ server/routes/twitterRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getTweet, createTweet } from '../controllers/tweets.js';
+import { getTweet, createTweet, testTweet } from '../controllers/tweets.js';
 
 const router = express.Router();
 
 router.get('/', getTweet);
+router.get('/test', testTweet);
 router.post('/', createTweet);
 
 export default router;
